fa2faf474feaeaf88c695d059db8700e1a13d0a3
Rahul <87407780+wreck2053@users.noreply.github.com>
2024-03-06 19:13:46 +0530

    Added Login Page Designs

---
+++ constants.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  transparent: '#00000000',
+  white: '#fafafa',
+  black: '#000000',
+  dark_peach: '#D19641',
+  light_peach: '#faecd4',
+  light_gray: '#CCCCCC',
+};
+
+export const FONT_SIZES = {
+  small: 12,
+  regular: 16,
+  medium: 18,
+  large: 24,
+  extraLarge: 32,
+};
